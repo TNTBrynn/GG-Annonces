dba3e0b4b57eb4d836e800670f909caaa532f9f3
SnowB0mb <patrysimon@videotron.ca>
2024-04-22 14:31:42 -0400

    fichier

---
+++ navigation.php
@@ -0,0 +1,29 @@
+<head>
+<link rel="stylesheet" href="projet2PDO.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark static-top">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="../entreprise/entreprise.php">Entreprises</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../typeAdr/typeAdr.php">Type Adresse</a>
+      </li>
+      <li>
+        <a class="nav-link" href="../adresse/adresse.php">Adresse</a>
+      </li>
+      <li>
+        <a class="nav-link" href="../employe/employee.php">Employee</a>
+      </li>
+      <li>
+        <a class="nav-link" href="../typeTel/typeTel.php">Type de Tel</a>
+      </li>
+      <li>
+        <a class="nav-link" href="../tel/tel.php">Téléphone</a>
+      </li>
+      <li>
+        <a class="nav-link" href="../deconnexion.php">Déconnexion</a>
+      </li>
+    </ul>
+  </nav>
