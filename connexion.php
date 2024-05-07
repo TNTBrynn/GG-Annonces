ffdf785bc41d0c8cf33df5cc3cb83927af6347e9
marid1 <marilyn1109@icloud.com>
2024-05-07 12:36:26 -0400

    les regex sont fix

--- connexion.php
+++ connexion.php
@@ -19,7 +19,7 @@ require_once ("navigationConn.php");
 <script type="text/javascript">
     $(document).ready(function () {
         $("#btnConnexion").click(function () {
-            var exprRegEmail = /^[a-zA-Z0-9]*@[a-z]+\.[a-z]{3}$/i ;///^[a-z]+@[a-z]+\.[a-z]{3}$/i; 
+            var exprRegEmail = /^[a-z0-9.]*@[a-z]+\.[a-z]{3}$/i;
             var exprRegMDP = /^[a-z0-9]{5,15}$/i;
             var strEmail = $("#tbEmail").val();
             var strMDP = $("#tbMDP").val();
