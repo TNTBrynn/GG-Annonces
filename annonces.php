d0d3bb9b4ed31d0621a730073a87febfd67136ac
SnowB0mb <patrysimon@videotron.ca>
2024-05-10 12:51:41 -0400

    ajout de la barre de recherche a annonces.php

--- annonces.php
+++ annonces.php
@@ -1,78 +1,100 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <?php
+    session_start();
+    if (!isset($_SESSION['session']) || $_SESSION['session'] != session_id()) {
+        //On redirige vers la page de connexion si la session n'existe pas ou si la session n'est pas égale à la session_id()
+        header('Location: ../connexion.php');
+    } else {
+        require_once('connect.php');
+        $email = $_SESSION['Courriel'];
 
-<?php
-session_start();
-if (!isset($_SESSION['session']) || $_SESSION['session'] != session_id()) {
-    //On redirige vers la page de connexion si la session n'existe pas ou si la session n'est pas égale à la session_id()
-    header('Location: ../connexion.php');
-} else {
-require_once('connect.php');
-$email = $_SESSION['Courriel'];
+        $sql = "SELECT * FROM utilisateurs WHERE Courriel = :Courriel";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([':Courriel' => $id]);
 
-$sql = "SELECT * FROM utilisateurs WHERE Courriel = :Courriel";
-$stmt = $db->prepare($sql);
-$stmt->execute([':Courriel' => $id]);
+        if ($stmt->rowCount() > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (empty($row['Prenom']) || empty($row['Nom'])) {
+                // header("Location: profil.php?id=$id");
+                exit;
+            }
+        }
+    ?>
+        <title>Annonces GG GMLS</title>
+        <style>
+            .ok {
+                background: url("images/bliss.jpg") no-repeat center center fixed;
+                font-family: Arial, sans-serif
+            }
 
-if ($stmt->rowCount() > 0) {
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    if (empty($row['Prenom']) || empty($row['Nom'])) {
-        // header("Location: profil.php?id=$id");
-        exit;
-    }
-}
-?>
-    <title>Annonces GG GMLS</title>
-    <style>
+            .grid-container {
+                display: grid;
+                grid-template-columns: auto auto auto auto;
+                padding: 10px;
+            }
 
-    .ok {
-        background: url("images/bliss.jpg") no-repeat center center fixed;
-        font-family: Arial, sans-serif
-    }
-    .grid-container {
-        display: grid;
-        grid-template-columns: auto auto auto auto;
-        padding: 10px;
-    }
-    .grid-item {
-        border: 1px solid rgba(0, 0, 0, 0.8);
-        padding: 20px;
-        font-size: 30px;
-        text-align: center;
-        background: url("images/tuile.jpg");
-    }
-    .grid-item img {
-        width: 300px;
-        height: 300px;
-    }
-    h1 {
-        font-size: 50px;
-        text-align: center;
-        margin: 50px auto;
-    }
-</style>
-<?php require_once 'navigation.php'; ?>
+            .grid-item {
+                border: 1px solid rgba(0, 0, 0, 0.8);
+                padding: 20px;
+                font-size: 30px;
+                text-align: center;
+                background: url("images/tuile.jpg");
+            }
+
+            .grid-item img {
+                width: 300px;
+                height: 300px;
+            }
+
+            h1 {
+                font-size: 50px;
+                text-align: center;
+                margin: 50px auto;
+            }
+        </style>
+        <?php require_once 'navigation.php'; ?>
 </head>
+
 <body class="ok">
     <h1>Annonces GG</h1>
+    <div>
+        <form method="get">
+            <input type="text" name="search" placeholder="Rechercher...">
+            <button type="submit">Rechercher</button>
+        </form>
+    </div>
     <div class="grid-container">
-<?php
-    // Check if the 'id' parameter is present in the URL
+        <?php
+        // Check if the 'id' parameter is present in the URL
         // Fetch the 'id' value from the URL
-        // $id = $_GET['id'];
 
         // Fetch the data from the database
-        require_once 'connect.php';
-         $sql = "SELECT * FROM annonces";
-            $result = $db->query($sql);
-            $items2 = $result->fetchAll(PDO::FETCH_ASSOC);
+        if (isset($_GET['search'])) {
+            $search = $_GET['search'];
+            $searchTerm = "%" . $search . "%";
+            
+            $sql = "SELECT * FROM `annonces` A 
+            INNER JOIN `utilisateurs` U ON U.NoUtilisateur = A.NoUtilisateur 
+            INNER JOIN `categories` C ON C.NoCategorie = A.Categorie
+            WHERE A.DescriptionAbregee LIKE :searchTerm 
+            OR A.DescriptionComplete LIKE :searchTerm 
+            OR U.Nom LIKE :searchTerm  
+            OR U.Prenom LIKE :searchTerm  
+            OR C.Description LIKE :searchTerm";
 
-        // Display the data raw
-        ?>
-        <script>console.log(<?php var_dump($items2) ?>);</script>
-        <?php
-        
+            $query = $db->prepare($sql);
+            $query->bindParam(':searchTerm', $searchTerm, PDO::PARAM_STR);
+            $query->execute();
+            $items2 = $query->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            $sql = "SELECT * FROM annonces";
+            $query = $db->prepare($sql);
+            $query->execute();
+            $items2 = $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+
         // Assuming you have an array of images and descriptions
         $items = [
             ['img' => 'images/car.jpg', 'desc' => 'Voici ma voiture chéri, elle s\'appelle "La Bête", besoin d\'amour, mais en bonne condition'],
@@ -83,33 +105,35 @@ if ($stmt->rowCount() > 0) {
             ['img' => 'images/rock.jpg', 'desc' => 'Voici The Rock, il est cool']
         ];
 
-foreach ($items2 as $index => $item) {
-    // Fetch the author's full name
-    $sql = "SELECT Nom, Prenom FROM utilisateurs WHERE NoUtilisateur = :NoUtilisateur";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([':NoUtilisateur' => $item['NoUtilisateur']]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-    $fullName = $user['Nom'] . ' ' . $user['Prenom'];
+        foreach ($items2 as $index => $item) {
+            // Fetch the author's full name
+            $sql = "SELECT Nom, Prenom FROM utilisateurs WHERE NoUtilisateur = :NoUtilisateur";
+            $stmt = $db->prepare($sql);
+            $stmt->execute([':NoUtilisateur' => $item['NoUtilisateur']]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            $fullName = $user['Nom'] . ' ' . $user['Prenom'];
 
-    // Fetch the category description
-    $sql = "SELECT Description FROM categories WHERE NoCategorie = :NoCategorie";
-    $stmt = $db->prepare($sql);
-    $stmt->execute([':NoCategorie' => $item['Categorie']]);
-    $category = $stmt->fetch(PDO::FETCH_ASSOC);
+            // Fetch the category description
+            $sql = "SELECT Description FROM categories WHERE NoCategorie = :NoCategorie";
+            $stmt = $db->prepare($sql);
+            $stmt->execute([':NoCategorie' => $item['Categorie']]);
+            $category = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    echo '<div class="grid-item">';
-    echo '<h2>Item ' . ($index + 1) . '</h2>';
-    echo '<p>No Annonce: ' . $item['NoAnnonce'] . '</p>';
-    echo '<p>Date d\'ajout: ' . $item['Parution'] . '</p>';
-    echo '<p>Auteur: ' . $fullName . '</p>';
-    echo '<p>Categorie: ' . $category['Description'] . '</p>';
-    echo '<p><a href="description.php?id=' . $item['NoAnnonce'] . '">' . $item['DescriptionAbregee'] . '</a></p>';
-    echo '<p>Prix: ' . $item['Prix'] . ' $ CAD</p>';
-    echo '<img src="' . $item['Photo'] . '" alt="' . $item['DescriptionComplete'] . '">';
-    echo '</div>';
-}       
-}
-?>
+            echo '<div class="grid-item">';
+            echo '<h2>Item ' . ($index + 1) . '</h2>';
+            echo '<p>No Annonce: ' . $item['NoAnnonce'] . '</p>';
+            echo '<p>Date d\'ajout: ' . $item['Parution'] . '</p>';
+            echo '<p>Auteur: ' . $fullName . '</p>';
+            echo '<p>Categorie: ' . $category['Description'] . '</p>';
+            echo '<p><a href="description.php?id=' . $item['NoAnnonce'] . '">' . $item['DescriptionAbregee'] . '</a></p>';
+            echo '<p>Prix: ' . $item['Prix'] . ' $ CAD</p>';
+            echo '<img src="' . $item['Photo'] . '" alt="' . $item['DescriptionComplete'] . '">';
+            echo '</div>';
+        }
+    }
+    require_once('close.php');
+    ?>
     </div>
 </body>
+
 </html>
